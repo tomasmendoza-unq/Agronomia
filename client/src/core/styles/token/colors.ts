55cbbb7ea591b9e497497f4d6f73ab8d5158e08d
Nicolás Bossi <nbossi@hotmail.com>
2026-07-28 19:03:36 -0300

    Se agregó el token del color con los colores para el botón y para el fondo general

---
+++ client/src/core/styles/token/colors.ts
@@ -0,0 +1,8 @@
+import { defineTokens } from "@pandacss/dev";
+
+const tokenColors = defineTokens.colors({
+    greenColor: { value: '#9AC638' },
+    surfacePageColor: { value: '#F9FAFB' }
+});
+
+export default tokenColors;
