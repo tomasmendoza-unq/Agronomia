1f9042f021107d53f69e9fa749140d3a577704fb
Nicolás Bossi <nbossi@hotmail.com>
2026-07-28 19:04:04 -0300

    Se agregó el color del fondo para el login

--- client/src/features/login/pages/init-login/styles.ts
+++ client/src/features/login/pages/init-login/styles.ts
@@ -1,10 +1,12 @@
 import { css } from "@styled-system/css";
+import { token } from "@styled-system/tokens";
 
 const styles = css({
     display: 'grid',
     placeItems: 'center',
     width: '100vw',
-    height: '100vh'
+    height: '100vh',
+    bg: token("colors.surfacePageColor")
 });
 
 export default styles;
