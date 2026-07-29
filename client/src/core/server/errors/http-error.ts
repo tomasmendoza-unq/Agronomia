ff6a2fcf4a06f74857824c2c8fc4ffb98c5026e6
Nicolás Bossi <nbossi@hotmail.com>
2026-07-29 10:02:59 -0300

    Se crearon los tipos para los errores y la clase para encapsular los datos expuestos para las funcionalidades

---
+++ client/src/core/server/errors/http-error.ts
@@ -0,0 +1,41 @@
+import type { ErrorResponse } from "../types/error-response";
+import type { ErrorSide } from "../types/error-side";
+
+export class HttpError extends Error {
+    private data: ErrorResponse
+    private status: number;
+    private side: ErrorSide;
+
+    constructor(data: ErrorResponse, status: number) {
+        super(data.message);
+        this.data = data;
+        this.status = status;
+        this.side = this.errorSide(status);
+    }
+
+    get getCause() {
+        return this.data.cause
+    }
+
+    get getTitle() {
+        return this.data.title
+    }
+
+    get getMessage() {
+        return this.data.message
+    }
+
+    get getStatus() {
+        return this.status
+    }
+
+    get getSideError() {
+        return this.side
+    }
+
+    private errorSide(status: number) {
+        if      (status >= 500) return 'Server';
+        else if (status >= 400) return 'Client';
+        else                    return 'Uknow';
+    }
+}
---
+++ client/src/core/server/types/error-response.ts
@@ -0,0 +1,12 @@
+import type { ErrorCause } from "./error-cause";
+import type { ErrorMotive } from "./error-motive";
+
+export interface ErrorResponse {
+    id: string;
+    title: string;
+    message: string;
+    path: string;
+    timestamp: Date;
+    cause: ErrorCause;
+    motives?: ErrorMotive[]
+}
