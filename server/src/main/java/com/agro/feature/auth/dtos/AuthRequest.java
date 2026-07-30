bf10d13596720dde02ac1b20ff4199966c1fea39
Nicolás Bossi <nbossi@hotmail.com>
2026-07-30 17:22:16 -0300

    Se instaló los validadores de jakarta para el body de las request

--- server/src/main/java/com/agro/feature/auth/dtos/AuthRequest.java
+++ server/src/main/java/com/agro/feature/auth/dtos/AuthRequest.java
@@ -1,4 +1,11 @@
 package com.agro.feature.auth.dtos;
 
-public record AuthRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record AuthRequest(
+        @Email @NotNull @NotBlank String email,
+        @NotNull @NotBlank String password
+) {
 }
