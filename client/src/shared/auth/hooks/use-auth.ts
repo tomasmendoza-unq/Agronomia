061748f6652bed9bcedb701ef6b1f3e79b75176b
Nicolás Bossi <nbossi@hotmail.com>
2026-07-30 15:54:33 -0300

    Se agregó validación para el contexto y se creó el servicio para realizar el inicio de sesión

--- client/src/shared/auth/hooks/use-auth.ts
+++ client/src/shared/auth/hooks/use-auth.ts
@@ -1,11 +1,23 @@
 import type { User } from "@/shared/domain/user/user"
 import type { Credentials } from "../types/credentials"
 import type { HttpError } from "@/core/server/errors/http-error"
+import { useContext } from "react"
+import AuthContext from "../context/auth.context"
+
+export const useAuth = () => {
+    const context = useContext(AuthContext)
+
+    if(!context) {
+        throw new Error('El contexto no está siendo colocado para el suscriptor');
+    }
+
+    return context;
+}
 
 export interface UseAuth {
-    user: User | null 
+    user: User | undefined 
     isLoading: boolean 
-    error: HttpError | null
-    login: Promise<(credentials: Credentials) => Promise<void>> 
-    logout: Promise<(credentials: Credentials) => Promise<void>> 
+    error: HttpError | undefined
+    login: (credentials: Credentials) => Promise<void>
+    logout: (credentials: Credentials) => Promise<void>
 }
--- client/src/shared/auth/services/login.ts
+++ client/src/shared/auth/services/login.ts
@@ -1,8 +1,10 @@
 import type { User } from "@/shared/domain/user/user";
 import type { Credentials } from "../types/credentials";
+import http from "@/core/server/http-client";
 
-function login(credentials: Credentials): Promise<User> {
-
+async function login(credentials: Credentials): Promise<User> {
+    const response = await http.post<User>('api/v1/login', credentials);
+    return response.data;
 }
 
 export default login;
