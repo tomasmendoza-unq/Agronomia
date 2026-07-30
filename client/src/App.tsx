f2945a09474169c72362fd9494c1316d9df32aca
Nicolás Bossi <nbossi@hotmail.com>
2026-07-30 15:53:36 -0300

    Se agregó el contexto de autorización para todos los componentes y se utilizó la función de login en el componente de Login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import { RouterProvider } from "react-router"
 import { routes } from "./core/routes/create-routes"
+import AuthProvider from "./shared/auth/context/auth.provider";
 
 function App() {
   return (
-    <RouterProvider router = {routes} />
+    <AuthProvider>
+      <RouterProvider router = {routes} />
+    </AuthProvider>
   )
 }
 
--- client/src/features/login/pages/init-login/Login.tsx
+++ client/src/features/login/pages/init-login/Login.tsx
@@ -3,14 +3,18 @@ import styles from "./styles";
 import loginButton from "./types/button";
 import loginInputs from "./types/inputs";
 import loginLinks from "./types/links";
+import { useAuth } from "@/shared/auth/hooks/use-auth";
 
 const Login = () => {
+
+    const { login } = useAuth()
+
     return (
         <section className = {styles}>
             <CredentialsForm 
                 title = "Bienvenido"
                 inputs = {loginInputs}
-                button = {loginButton(() => console.log("Hola"))} //button = {loginButton} // pasar por parametro la funcionalidad al apretar botón
+                button = {loginButton(login)}
                 links = {loginLinks}
             />
         </section>
