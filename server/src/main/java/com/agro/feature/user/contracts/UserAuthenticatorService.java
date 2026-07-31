8d666db0b8b9e199fc3af1adb686cdef67ebf516
Nicolás Bossi <nbossi@hotmail.com>
2026-07-31 12:48:09 -0300

    Se crearon tests para el usuario

--- server/src/main/java/com/agro/feature/user/contracts/UserAuthenticatorService.java
+++ server/src/main/java/com/agro/feature/user/contracts/UserAuthenticatorService.java
@@ -1,4 +1,7 @@
 package com.agro.feature.user.contracts;
 
+import com.agro.shared.entities.UserAuthenticate;
+
 public interface UserAuthenticatorService {
+    UserAuthenticate getCredentialsByEmail(String email);
 }
