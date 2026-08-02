07a773639dd2c801f2abb70e6dbee73902ea2ce5
Nicolás Bossi <nbossi@hotmail.com>
2026-08-02 15:39:44 -0300

    Se creó un servicio que tiene el manager y que devuelve una implementación de userdetails

--- server/src/main/java/com/agro/feature/auth/orchestrator/CredentialsLoginOrchestratorImpl.java
+++ server/src/main/java/com/agro/feature/auth/orchestrator/CredentialsLoginOrchestratorImpl.java
@@ -2,33 +2,26 @@ package com.agro.feature.auth.orchestrator;
 
 import com.agro.feature.auth.domain.Auth;
 import com.agro.feature.auth.services.jwt.JwtService;
+import com.agro.feature.auth.services.userDetails.UserCredentials;
+import com.agro.feature.auth.services.validate.AuthService;
 import com.agro.shared.entities.Credentials;
-import com.agro.feature.user.contracts.UserCredentialsService;
-import com.agro.shared.entities.UserAuthenticate;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CredentialsLoginOrchestratorImpl implements LoginOrchestrator {
 
-    private AuthenticationManager authenticatorManager;
-    private UserCredentialsService credentialsService;
+    private AuthService authService;
     private JwtService jwtService;
 
-    public CredentialsLoginOrchestratorImpl(AuthenticationManager authenticatorManager, UserCredentialsService userService, JwtService jwtService) {
-        this.authenticatorManager = authenticatorManager;
-        this.credentialsService = userService;
+    public CredentialsLoginOrchestratorImpl(AuthService authService, JwtService jwtService) {
+        this.authService = authService;
         this.jwtService = jwtService;
     }
 
     @Override
-    public Auth auth(Credentials credentials) {
-        UserAuthenticate authenticate = credentialsService.getCredentialsByEmail(credentials.email());
-        authenticatorManager.authenticate(
-                new UsernamePasswordAuthenticationToken(credentials.email(), credentials.password())
-        );
-        String token = jwtService.generate(authenticate);
-        return new Auth(authenticate.name(), authenticate.email(), token, authenticate.id());
+    public Auth login(Credentials credentials) {
+        UserCredentials userCredentials = authService.login(credentials);
+        String token = jwtService.generate(userCredentials);
+        return new Auth(userCredentials.getName(), userCredentials.getEmail(), token, userCredentials.getId());
     }
 }
