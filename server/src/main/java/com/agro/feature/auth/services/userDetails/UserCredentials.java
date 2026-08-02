8e41305a1fc525bde282011ecbaf96eff941849b
Nicolás Bossi <nbossi@hotmail.com>
2026-08-02 15:40:25 -0300

    Se implementó una clase para la interfaz userDetails y se devuelve una instancia en userDetailsServices

---
+++ server/src/main/java/com/agro/feature/auth/services/userDetails/UserCredentials.java
@@ -0,0 +1,56 @@
+package com.agro.feature.auth.services.userDetails;
+
+import com.agro.shared.entities.UserAuthenticate;
+import lombok.Getter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserCredentials implements UserDetails {
+
+    @Getter
+    private String email;
+
+    @Getter
+    private String password;
+
+    @Getter
+    private String role;
+
+    @Getter
+    private String name;
+
+    @Getter
+    private Long id;
+
+    private List<GrantedAuthority> roles = new ArrayList<>();
+
+    public UserCredentials(UserAuthenticate user) {
+        this.email = user.email();
+        this.password = user.password();
+        this.role = user.role();
+        this.name = user.name();
+        this.id = user.id();
+        this.roles.add(new SimpleGrantedAuthority(user.role()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
--- server/src/main/java/com/agro/feature/auth/services/userDetails/UserDetailsServiceImpl.java
+++ server/src/main/java/com/agro/feature/auth/services/userDetails/UserDetailsServiceImpl.java
@@ -1,17 +1,24 @@
 package com.agro.feature.auth.services.userDetails;
 
+import com.agro.feature.user.contracts.UserCredentialsService;
+import com.agro.shared.entities.UserAuthenticate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 public class UserDetailsServiceImpl implements UserDetailsAdapter {
+
+    private UserCredentialsService userCredentialsService;
+
+    public UserDetailsServiceImpl(UserCredentialsService userCredentialsService) {
+        this.userCredentialsService = userCredentialsService;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        UserAuthenticate userAuthenticate = userCredentialsService.getCredentialsByEmail(username);
+        return new UserCredentials(userAuthenticate);
     }
 
     @Override
