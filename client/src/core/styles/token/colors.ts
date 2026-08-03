4821f00dc53a8dd7434d6a0e49d7850acacff288
Nicolás Bossi <nbossi@hotmail.com>
2026-08-03 17:23:13 -0300

    Se agregó un nuevo color como token

--- client/src/core/styles/token/colors.ts
+++ client/src/core/styles/token/colors.ts
@@ -4,6 +4,7 @@ const tokenColors = defineTokens.colors({
     greenColor: { value: "#9AC638" },
     surfacePageColor: { value: "#F9FAFB" },
     surfaceCardColor: { value: "#FFFFFF" },
+    danger: { value: "#E11D48" },
 });
 
 export default tokenColors;
