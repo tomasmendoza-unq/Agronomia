85d25823f78efc1cb4196738117a5512837d68e5
Tomas Mendoza <tm1453766@gmail.com>
2026-08-03 04:02:54 -0300

    feature: se implementa la creacion de usuarios con el hook, service y form modal

--- client/src/shared/components/table/Table.tsx
+++ client/src/shared/components/table/Table.tsx
@@ -1,5 +1,18 @@
 import type { TablePaginator } from "@/shared/types/table/Table";
-import { tableWrapper, table, thead, th, tbody, tr, td } from "./styles";
+import {
+    tableWrapper,
+    tableCard,
+    table,
+    thead,
+    th,
+    tbody,
+    tr,
+    td,
+    tdActions,
+    pagination,
+    paginationButton,
+    footerText,
+} from "./style";
 
 interface TableProps<T> extends TablePaginator<T> {
     onPageChange?: (page: number) => void;
@@ -10,7 +23,6 @@ export const Table = <T extends Record<string, unknown>>({
     headers = [],
     rows = [],
     page = 0,
-    size = 0,
     totalElements = 0,
     totalPages = 0,
     last = true,
@@ -19,56 +31,63 @@ export const Table = <T extends Record<string, unknown>>({
 }: TableProps<T>) => {
     return (
         <div className={tableWrapper}>
-            <table className={table}>
-                <thead className={thead}>
-                    <tr>
-                        {headers.map((header) => (
-                            <th
-                                key={header}
-                                className={th}
-                            >
-                                {header}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody className={tbody}>
-                    {rows.map((row) => (
-                        <tr
-                            key={row.id}
-                            className={tr}
-                        >
-                            {Object.keys(row.data).map((key) => (
-                                <td
-                                    key={key}
-                                    className={td}
+            <div className={tableCard}>
+                <table className={table}>
+                    <thead className={thead}>
+                        <tr>
+                            {headers.map((header) => (
+                                <th
+                                    key={header}
+                                    className={th}
                                 >
-                                    {renderCell
-                                        ? renderCell(row.data, key as keyof T)
-                                        : String(row.data[key as keyof T])}
-                                </td>
+                                    {header}
+                                </th>
                             ))}
-                            {row.actions && (
-                                <td className={td}>{row.actions}</td>
-                            )}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className={tbody}>
+                        {rows.map((row) => (
+                            <tr
+                                key={row.id}
+                                className={tr}
+                            >
+                                {Object.keys(row.data).map((key) => (
+                                    <td
+                                        key={key}
+                                        className={td}
+                                    >
+                                        {renderCell
+                                            ? renderCell(
+                                                  row.data,
+                                                  key as keyof T,
+                                              )
+                                            : String(row.data[key as keyof T])}
+                                    </td>
+                                ))}
+                                {row.actions && (
+                                    <td className={tdActions}>{row.actions}</td>
+                                )}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
 
             {onPageChange && (
-                <div className={tableWrapper}>
+                <div className={pagination}>
                     <button
+                        className={paginationButton}
                         disabled={page === 0}
                         onClick={() => onPageChange(page - 1)}
                     >
                         Anterior
                     </button>
-                    <span>
+                    <span className={footerText}>
                         Página {page + 1} de {totalPages} ({totalElements}{" "}
                         elementos)
                     </span>
                     <button
+                        className={paginationButton}
                         disabled={last}
                         onClick={() => onPageChange(page + 1)}
                     >
@@ -76,6 +95,10 @@ export const Table = <T extends Record<string, unknown>>({
                     </button>
                 </div>
             )}
+
+            <span className={footerText}>
+                Mostrando {rows.length} de {totalElements} usuarios
+            </span>
         </div>
     );
 };
