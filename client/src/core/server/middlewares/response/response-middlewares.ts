ff484147bf3ecccefbd3ef44db37ab7f4f5fcf00
Nicolás Bossi <nbossi@hotmail.com>
2026-08-03 21:45:40 -0300

    Se cambió de orden el middleware para que tome el error directo de la response

--- client/src/core/server/middlewares/response/response-middlewares.ts
+++ client/src/core/server/middlewares/response/response-middlewares.ts
@@ -3,8 +3,8 @@ import handlerErrorMiddleware from "./handler-error.middeware";
 import handlerLoggerMiddleware from "./handler-logger.middleware";
 
 function setResponseMiddlewares(http: AxiosInstance) {
-    handlerErrorMiddleware(http);
     handlerLoggerMiddleware(http);
+    handlerErrorMiddleware(http);
 }
 
 export default setResponseMiddlewares;
