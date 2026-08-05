eb484c1dd34727763f9bb2c65773ee9f23dbb0e3
Nicolás Bossi <nbossi@hotmail.com>
2026-08-04 22:21:48 -0300

    Se habilitaron las funcionalidades propias de typescript para agregar enums para crear roles

--- client/src/shared/domain/user/user.ts
+++ client/src/shared/domain/user/user.ts
@@ -1,8 +1,9 @@
 import type { CompanyData } from "@/features/company/types/CompanyData.t";
+import type { Role } from "./role";
 
 export interface User {
     id: number;
     email: string;
-    rol: string;
+    rol: Role;
     company: CompanyData;
 }
