4d2211939132d791a30f1ebf3672a42a1cbd58b2
Nicolás Bossi <nbossi@hotmail.com>
2026-08-06 20:13:31 -0300

    Se modificó el provedor para que obtenga nuevamente los datos del usuario al recargar la página

--- client/src/features/auth/layout/AuthenticatedLayout.tsx
+++ client/src/features/auth/layout/AuthenticatedLayout.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router";
+import { Outlet } from "react-router";
 import NavBar from "../../../shared/components/navbar/Navbar";
 import { links } from "./types/links";
 import Breadcrumb from "@/shared/components/breadcrumb/Breadcrumb";
@@ -6,31 +6,24 @@ import { authLayout } from "./styles";
 import Brand from "./components/Brand";
 import Avatar from "./components/Avatar";
 import { useAuth } from "../hooks/use-auth";
+import { ProtectedRoute } from "@/shared/routes/ProtectedRoute";
 
 const AuthenticatedLayout = () => {
     const { user, isAuthenticated } = useAuth();
 
-    if (!isAuthenticated) {
-        return (
-            <Navigate
-                to="/login"
-                replace
-            />
-        )
-    }
-
-    else if (!user) return null;
-
     return (
-        <main className={authLayout}>
-            <NavBar
-                brand={<Brand companyLogo={user.companyLogo} />}
-                avatar={<Avatar avatar={user} />}
-                links={links}
-            />
-            <Breadcrumb />
-            <Outlet />
-        </main>
+        <ProtectedRoute isAuthenticated={isAuthenticated}>
+            {user &&
+            <main className={authLayout}>
+                <NavBar
+                    brand={<Brand companyLogo={user.companyLogo} />}
+                    avatar={<Avatar avatar={user} />}
+                    links={links}
+                />
+                <Breadcrumb />
+                <Outlet />
+            </main>}
+        </ProtectedRoute>
     );
 };
 
