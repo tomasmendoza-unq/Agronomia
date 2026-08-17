3feccabe8a8673eaa15ef548e3b1974fcf35e4da
Tomas Mendoza <tm1453766@gmail.com>
2026-08-17 13:41:21 -0300

    rf: se pasa a inputRow el edit de company!

--- client/src/features/admin/pages/configuration/components/company/components/form/types/inputs.ts
+++ client/src/features/admin/pages/configuration/components/company/components/form/types/inputs.ts
@@ -1,34 +1,40 @@
-import type { InputData } from "../../../../../../../../../shared/components/credentials-form/types/input/credentials-input";
+import type { InputRow } from "@/shared/components/form/insert-form";
 
-const createUserInputs: InputData[] = [
-    {
-        type: "text",
-        name: "name",
-        title: "Nombre",
-        placeholder: "",
-        id: 0,
-    },
-    {
-        type: "text",
-        name: "legalName",
-        title: "Razon social",
-        placeholder: "",
-        id: 1,
-    },
-    {
-        type: "text",
-        name: "cuit",
-        title: "CUIT",
-        placeholder: "Ingrese CUIT",
-        id: 2,
-    },
-    {
-        type: "file",
-        name: "logo",
-        title: "Logo",
-        placeholder: "",
-        id: 3,
-    },
+const createUserInputs: InputRow[] = [
+    [
+        {
+            type: "text",
+            name: "name",
+            title: "Nombre",
+            placeholder: "",
+            id: 0,
+        },
+    ],
+    [
+        {
+            type: "text",
+            name: "legalName",
+            title: "Razon social",
+            placeholder: "",
+            id: 1,
+        },
+        {
+            type: "text",
+            name: "cuit",
+            title: "CUIT",
+            placeholder: "Ingrese CUIT",
+            id: 2,
+        },
+    ],
+    [
+        {
+            type: "file",
+            name: "logo",
+            title: "Logo",
+            placeholder: "",
+            id: 3,
+        },
+    ],
 ];
 
 export default createUserInputs;
