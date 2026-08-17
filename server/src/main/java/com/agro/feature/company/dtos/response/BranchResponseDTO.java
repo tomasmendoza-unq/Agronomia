2b0f461525c55c75b1e48947791fc7b32de1e636
Tomas Mendoza <tm1453766@gmail.com>
2026-08-16 23:58:56 -0300

    ft: se agrega el id en el dto de branch

--- server/src/main/java/com/agro/feature/company/dtos/response/BranchResponseDTO.java
+++ server/src/main/java/com/agro/feature/company/dtos/response/BranchResponseDTO.java
@@ -1,4 +1,13 @@
 package com.agro.feature.company.dtos.response;
 
-public record BranchResponseDTO() {
+import com.agro.feature.branch.domain.Branch;
+
+public record BranchResponseDTO(
+        long id,
+        String city,
+        String street
+) {
+    public static BranchResponseDTO fromModel(Branch branch) {
+        return new BranchResponseDTO(branch.getId(), branch.getCity(), branch.getDirection());
+    }
 }
