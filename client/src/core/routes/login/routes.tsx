10d0dccb688c93f523b271a9626e2c5e43bd3623
Nicolás Bossi <nbossi@hotmail.com>
2026-08-26 12:35:49 -0300

    Se refactorizó la forma de enrutar los componentes del login para seguir el estandar del admin y el de facturacióbn

--- client/src/core/routes/login/routes.tsx
+++ client/src/core/routes/login/routes.tsx
@@ -2,10 +2,12 @@ import Login from "@/features/login/pages/init/Login";
 import type { RouteData } from "../route-data";
 import { LOGIN } from "./paths";
 
-const loginRoutes: RouteData = {
-    path: LOGIN.INIT,
-    element: <Login />,
-    children: []
-}
+const loginRoutes: RouteData[] = [
+    {
+        path: LOGIN.INIT,
+        element: <Login />,
+        children: []
+    }
+];
 
 export default loginRoutes;
