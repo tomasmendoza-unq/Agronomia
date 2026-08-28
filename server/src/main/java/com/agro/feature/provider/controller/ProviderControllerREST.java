f4ed8a4edb2f4f7fa345947525feaa0ce3555e46
Tomas Mendoza <tm1453766@gmail.com>
2026-08-27 23:06:00 -0300

    ft: se agrega documentacion swagger al endpoint de obtener proveedores

--- server/src/main/java/com/agro/feature/provider/controller/ProviderControllerREST.java
+++ server/src/main/java/com/agro/feature/provider/controller/ProviderControllerREST.java
@@ -3,7 +3,10 @@ package com.agro.feature.provider.controller;
 import com.agro.core.api.Api;
 import com.agro.feature.provider.contracts.ProviderDataService;
 import com.agro.feature.provider.dtos.response.ProviderResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,7 @@ import java.util.List;
 @RestController
 @Slf4j
 @RequestMapping(Api.PROVIDER)
+@Tag(name = "Proveedores", description = "Operaciones relacionadas a la gestión de proveedores")
 public class ProviderControllerREST {
 
     private final ProviderDataService providerDataService;
@@ -22,6 +26,8 @@ public class ProviderControllerREST {
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('DUENIO', 'VENDEDOR')")
+    @Operation(summary = "Obtener los proveedores de forma paginada", description = "Devuelve los proveedores paginados.")
     public List<ProviderResponseDTO> getProviders() {
         return providerDataService.getProviders().stream().map(ProviderResponseDTO::fromModel).toList();
     }
