3ac656f662573bafcc1c66c52e577e74ff0903d0
Nicolás Bossi <111707809+nicobossi@users.noreply.github.com>
2026-08-30 16:15:43 -0300

    Feature/add client (#53)
    * Se cambió el contrato de los inputs para utilizar en todos los formularios
    
    * Se quitó el id como propiedad del sub form
    
    * Se creó un componente test para probar el formulario generico para los paneles de provedores y clientes
    
    * Se agregaron los inputs para utilizar en todos los tipos de formularios
    
    * Se creó el formulario de validación de inputs
    
    * Se creó el formulario simple
    
    * Se creó un componente para gestionar la cantidad de botones de cada formulario de forma dínamica
    
    * Se parametrizó el evento para cerrar la modal con el botón de cancelación opcional
    
    * Se modificó el color de la modal y se corrigió el nombre de la propiedad para el botón de cancelación
    
    * Se eliminaron las implementaciones viejas de formularias
    
    * Se creó la vista para gestionar el flujo de las funcionalidades del cliente
    
    * Se asoció el componente con la funcionalidad de agregar un cliente a la ruta del cliente
    
    * Se creó un sobformulario para contener los inputs relacionados a determinada tematica
    
    * Se creó el esquema de validación para la persona natural
    
    * Se crearon los datos de los inputs para el subformulario de agregar un nuevo cliente
    
    * Se utilizó el formulario compuesto para agregar el formulario de los datos personales de un cliente
    
    * Se corrigieron asociaciones entre inputs y esquema y se agregaron validaciones en el campo de provincia
    
    * Se creó un formulario que muestra subformularios u otros dependiendo del subtipo seleccionado por el usuario
    
    * Se creó un input dinamico que aplica transformaciones del valor que coloca el usuario a tiempo real para mostrar los guiones del cuit
    
    * Se creó el esquema para la razón social con su respectivo tipo de esquema
    
    * Se creó el formulario para el cliente de razón social
    
    * Se agregó el subform faltante para el cliente de razón social
    
    * Se resolvieron conflictos de estilos y de importaciones
    
    ---------
    
    Co-authored-by: Nicolás Bossi <nbossi@hotmail.com>

--- client/src/features/add-client/pages/types/subforms.ts
+++ client/src/features/add-client/pages/types/subforms.ts
@@ -1,7 +1,7 @@
 import type { SubForm } from "@/shared/components/forms/types/sub-form";
 import { formatCuit } from "@/shared/domain/cuit-cuil/format";
 
-export const subForms: SubForm[] = [
+export const naturalPersonsubForms: SubForm[] = [
     {
         title: "Datos del cliente",
         inputs: [
@@ -72,5 +72,94 @@ export const subForms: SubForm[] = [
             ],
         ],
         id: 0
+    }
+];
+
+export const socialMotiveSubform: SubForm[] = [
+        {
+            title: "Datos cliente",
+            inputs: [
+            [
+                {
+                    type: "text",
+                    name: "name",
+                    title: "Nombre/Razón Social",
+                    placeholder: "Ingrese el nombre",
+                    id: 0,
+                },
+                {
+                    type: "dynamic",
+                    name: "cuit",
+                    title: "CUIT/CUIL",
+                    placeholder: "Ingrese el CUIT/CUIL",
+                    format: formatCuit,
+                    id: 2,
+                },
+            ],
+            [
+                {
+                    type: "text",
+                    name: "address",
+                    title: "Dirección",
+                    placeholder: "Ingrese la dirección",
+                    id: 5,
+                },
+            ],
+            [
+                {
+                    type: "text",
+                    name: "place",
+                    title: "Localidad",
+                    placeholder: "Ingrese la localidad",
+                    id: 6,
+                },
+                {
+                    type: "select",
+                    name: "provice",
+                    title: "Provincia",
+                    id: 7,
+                    options: []
+                },
+            ],
+        ],
+        id: 0,
+        },
+        {
+        title: "Razón social",
+        inputs: [
+            [
+                {
+                    type: "text",
+                    name: "name",
+                    title: "Nombre",
+                    placeholder: "Ingrese el nombre",
+                    id: 0,
+                },
+                {
+                    type: "text",
+                    name: "surname",
+                    title: "Apellido",
+                    placeholder: "Ingrese el apellido",
+                    id: 1,
+                },
+            ],
+            [
+                {
+                    type: "text",
+                    name: "phone",
+                    title: "Teléfono",
+                    placeholder: "Ingrese el teléfono",
+                    id: 3,
+                },
+                {
+                    type: "email",
+                    name: "email",
+                    title: "Email",
+                    placeholder: "Ingrese el correo",
+                    id: 4,
+                },
+            ],
+        ],
+        id: 1
     }
 ]
