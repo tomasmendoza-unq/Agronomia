bad5bf3eb06c37c7793b053b1f6a155da427fc52
Tomas Mendoza <tm1453766@gmail.com>
2026-09-01 20:57:50 -0300

    log api key

--- client/src/core/server/http-client.ts
+++ client/src/core/server/http-client.ts
@@ -4,6 +4,8 @@ import setRequestMiddlewares from "./middlewares/request/request-middlewares";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
+console.log("API_BASE_URL:", API_BASE_URL);
+
 const http = axios.create({
     baseURL: API_BASE_URL,
 });
