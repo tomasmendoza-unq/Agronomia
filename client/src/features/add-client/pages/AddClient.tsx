263e91a7a1c997e385696d9a13a858d191615102
Nicolás Bossi <nbossi@hotmail.com>
2026-09-01 12:50:22 -0300

    Se creó un custom hook para gestionar la petición del servicio

--- client/src/features/add-client/pages/AddClient.tsx
+++ client/src/features/add-client/pages/AddClient.tsx
@@ -2,13 +2,21 @@ import { naturalPersonsubForms, socialMotiveSubform } from "./types/subforms";
 import naturalPersonSchema from "./types/natural-person-schema";
 import PolimorficForm from "@/shared/components/forms/polimorfic-form/PolimorficForm";
 import socialMotiveSchema from "./types/social-motive-schema";
-import addClient from "../services/add-client";
-import useFetch from "@/shared/hooks/use-fetch/useFetch.hook";
 import ErrorToast from "@/shared/components/toast/error/ErrorToast";
+import useAddClient from "../hooks/use-add-client";
+import SuccessToast from "@/shared/components/toast/success/SuccessToast";
+import { useState } from "react";
+import type { ClientSchema } from "../adapter/client-schema";
 
 const AddClient = () => {
 
-    const {error, execute, refresh} = useFetch();
+    const {add, refresh, data, isError} = useAddClient();
+    const [client, setClient] = useState<ClientSchema>();
+
+    const handleClient = (clientData: ClientSchema) => {
+        setClient(clientData);
+        add(clientData);
+    }
 
     return (
         <>
@@ -18,19 +26,20 @@ const AddClient = () => {
                         subType: "razon social",
                         subforms: socialMotiveSubform,
                         schema: socialMotiveSchema,
-                        onSubmit: execute(addClient)
+                        onSubmit: handleClient
                     },
                     {
                         subType: "persona natural",
                         subforms: naturalPersonsubForms,
                         schema: naturalPersonSchema,
-                        onSubmit: execute(addClient)
+                        onSubmit: handleClient
                     }
                 ]}
                 buttonData={{text: "Agregar cliente"}}
                 onCancel={() => console.log("aa")}
             />
-            {error && <ErrorToast message={"aa"} onClose={refresh} />}
+            {isError && <ErrorToast message={`El cuit ${client?.cuit} ya está registrado`} onClose={refresh} />}
+            {data && <SuccessToast message={`Se ha creado el cliente ${client?.name} ${client?.surname}`} onClose={refresh} />}
         </>
     )
 }
