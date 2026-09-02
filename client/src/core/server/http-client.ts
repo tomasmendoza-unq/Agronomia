b8c2d250748f44d85605b049984625b34b6fcc57
Tomas Mendoza <tm1453766@gmail.com>
2026-09-01 21:09:57 -0300

    fix import env

--- client/src/core/server/http-client.ts
+++ client/src/core/server/http-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import setResponseMiddlewares from "./middlewares/response/response-middlewares";
 import setRequestMiddlewares from "./middlewares/request/request-middlewares";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 console.log("API_BASE_URL:", API_BASE_URL);
 
