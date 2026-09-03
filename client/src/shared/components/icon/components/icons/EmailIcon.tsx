f4c6d7ad684d92107e1fd86e491f86cca22c7688
Nicolás Bossi <nbossi@hotmail.com>
2026-09-03 15:44:01 -0300

    Se crearon los iconos para mostrar la información de cada tipo de cliente

---
+++ client/src/shared/components/icon/components/icons/EmailIcon.tsx
@@ -0,0 +1,18 @@
+const EmailIcon = () => {
+    return (
+        <svg width="15" height="12" viewBox="0 0 15 12" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.5 1.5L6 5.8L11.5 1.5V9.5H0.5V1.5Z"
+                fill="none"
+                stroke="#8D8D8D"
+                stroke-width="1.2"
+                stroke-linejoin="round"/>
+            <path d="M0.5 1L6 5.3L11.5 1"
+                    fill="none"
+                    stroke="#8D8D8D"
+                    stroke-width="1.2"
+                    stroke-linejoin="round"/>
+        </svg>
+    )
+}
+
+export default EmailIcon;
